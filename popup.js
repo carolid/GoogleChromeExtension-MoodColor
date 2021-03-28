8279750bd4495f82a219011b3602875fb251855a
Caroline D <cdavis469@gmail.com>
2021-03-28 11:03:49 -0400

    changed song function to account for bug

--- popup.js
+++ popup.js
@@ -200,7 +200,9 @@ for (emoji of emojis) {
       // *** Play song -- CAROLINE **
       // Modal Window Version
       var modal = document.getElementById("song_window");
+      modal = document.getElementById("song_window")
       var span = getElementById("close")[0];
+      modal.style.display = "block";
       modal.style.display = "block";
       window.oncick = function(event) {
           if (event.target == modal) {
