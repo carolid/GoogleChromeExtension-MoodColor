277253a74f10fbe7725942c0ce8afe2452030544
Caroline D <cdavis469@gmail.com>
2021-03-27 22:57:27 -0400

    v5

--- popup.js
+++ popup.js
@@ -200,7 +200,7 @@ for (emoji of emojis) {
     }
     let mood_id = this.id;
     let song_to_play = get_mood_song(this.id);
-    window.open(song_to_play);
+    window.open(result.song_to_play);
     })
   }
 
